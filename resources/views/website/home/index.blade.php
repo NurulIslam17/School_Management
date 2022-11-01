33047563515790fa2b034778226494fbc40ce031
Md Nurul Islam <nurulcse09@gmail.com>
2022-11-01 14:32:47 +0600

    Adding Slider in Website

--- resources/views/website/home/index.blade.php
+++ resources/views/website/home/index.blade.php
@@ -6,7 +6,14 @@
 
 
 @section('body')
-    <div class="carousel slide" data-bs-ride="carousel" dat-bs-interval="400">
+    <div class="carousel slide" id="slider" data-bs-ride="carousel" data-bs-interval="3800">
+
+        <ol class="carousel-indicators">
+            <li data-bs-target="#slider" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#slider" data-bs-slide-to="1" class=""></li>
+            <li data-bs-target="#slider" data-bs-slide-to="2" class=""></li>
+        </ol>
+
         <div class="carousel-inner">
 
             <div class="carousel-item active">
